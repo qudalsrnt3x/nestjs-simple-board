420ec1860952358da1ba0b42012072d3c2f02ff2
qudalsrnt3x <qudalsrnt3x@entwiz.com>
2023-10-23 14:22:09 +0900

    exceptionFilter를 사용하여 전역으로 에러응답코드 핸들링

---
+++ src/exceptions/http.exception.ts
@@ -0,0 +1,20 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { Response, Request } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptoinFilter implements ExceptionFilter {
+
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
+
+        response.status(status).json({
+            statusCode: status,
+            message: exception.message,
+            timestamp: new Date().toISOString(),
+            path: request.url
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptoinFilter } from './exceptions/http.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptoinFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 데코레이터가 없는 필드의 object는 거른다.
       forbidNonWhitelisted: true, // 정의되지 않은 필드에 대해서는 에러 처리
       transform: true,
     })
-  )
+  );
   await app.listen(3000);
 }
 bootstrap();
